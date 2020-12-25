0bcb41a9ebe69f65edc228fbb37c87f50fb56416
frimpongopoku <pongofrimi@gmail.com>
2020-12-25 08:53:45 +0000

    Initial Commit

---
+++ Shared/CustomSwiftViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftViewsApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
